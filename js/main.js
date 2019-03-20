7112125d8b06c069e0bfdfe0d63b3a059e5e5200
Ben Collier <ben.collier@xdevservices.com>
2019-03-20 15:34:24 -0400

    More popup testing

--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
 (function () {
   'use strict';
-  
+
+  const URL = 'https://benholio.github.io/instantplay.html';
+  const delay = 5000;
+  const debugDiv = document.getElementById('debug');
+
+  function logToPage(msg) {
+    let logEntry = document.createElement('p');
+    logEntry.className = 'log-entry';
+    logEntry.innerHTML = msg;
+    debugDiv.appendChild(logEntry);
+  }
+
   function openPopup() {
     window.console.log('will try to open popup in 5 seconds');
-    const URL = 'https://benholio.github.io/instantplay.html';
+    monitorNewWindow();
+    
     window.setTimeout(() => {
       window.console.log('trying to open popup via script');
-      let newWindow = window.open(URL, '', 'resizable=1');
-    }, 5000);
-  } 
+      logToPage('trying to open popup via script...');
+      window.newWindow = window.open(URL, '', 'resizable=1');
+      console.log('window.netWindow value right after opening: ' + window.newWindow);
+      logToPage('window.netWindow value right after opening: ' + window.newWindow);
+    }, delay);
+  }
+
+  function monitorNewWindow() {
+    let previousValue = 'xxx';
+    window.monitorInterval = window.setInterval(()=>{
+      if (previousValue !== window.newWindow) {
+        console.log('newWindow value changed: ' + window.newWindow);
+        previousValue = window.newWindow;
+      }
+    }, 33);
+  }
   
   function documentReady() {
     let meta = document.createElement('meta'); 
