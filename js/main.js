00b62c9313063c10b7a646116fd34529a89b5034
Ben Collier <bencollier78@gmail.com>
2019-03-20 15:09:01 -0400

    Popup testing

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
   'use strict';
   
   function openPopup() {
+    window.console.log('will try to open popup in 5 seconds');
     const URL = 'https://benholio.github.io/instantplay.html';
     window.setTimeout(() => {
       window.console.log('trying to open popup via script');
@@ -14,6 +15,7 @@
     meta.name = 'description'; 
     meta.content = 'This page is intended to test Google indexing of dynamically added meta descriptions.'; 
     document.head.insertBefore(meta, document.head.firstElementChild);
+    openPopup();
   }
   
   if(document.readyState === 'loading') {
