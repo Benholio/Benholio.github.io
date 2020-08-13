1565af07a944968ed6abf46af1a7272f63e7cc1a
Ben Collier <bencollier78@gmail.com>
2020-08-13 18:07:05 -0400

    Fooling around with fivey

--- js/main.js
+++ js/main.js
@@ -1,50 +1,22 @@
 (function () {
   'use strict';
-
-  const URL = 'https://benholio.github.io/instantplay.html';
-  const delay = 5000;
-  const debugDiv = document.getElementById('debug');
-
-  function logToPage(msg) {
-    let logEntry = document.createElement('p');
-    logEntry.className = 'log-entry';
-    logEntry.innerHTML = msg;
-    debugDiv.appendChild(logEntry);
-  }
-
-  function debugLog(msg) {
-    window.console.log(msg);
-    logToPage(msg);
-  }
-
-  function openPopup() {
-    debugLog('will try to open popup in ' + delay + ' milliseconds...');
-    monitorNewWindow();
-    
-    window.setTimeout(() => {
-      debugLog('trying to open popup...');
-      window.newWindow = window.open(URL, '', 'resizable=1');
-      console.log('window.netWindow value right after opening: ' + window.newWindow);
-      logToPage('window.open returned: ' + window.newWindow);
-    }, delay);
-  }
-
-  function monitorNewWindow() {
-    let previousValue = 'xxx';
-    window.monitorInterval = window.setInterval(()=>{
-      if (previousValue !== window.newWindow) {
-        console.log('newWindow value changed: ' + window.newWindow);
-        previousValue = window.newWindow;
-      }
-    }, 33);
-  }
   
   function documentReady() {
-    let meta = document.createElement('meta'); 
-    meta.name = 'description'; 
-    meta.content = 'This page is intended to test Google indexing of dynamically added meta descriptions.'; 
-    document.head.insertBefore(meta, document.head.firstElementChild);
-    openPopup();
+    const allFiveyTexts = document.querySelectorAll('.fivey-text');
+    const textArea = document.querySelector('textarea');
+    textArea.oninput = updateText;
+    textArea.focus();
+
+    function updateText(e) {
+      allFiveyTexts.forEach(function(fiveyText) {
+        if (e.target.value.length === 0 ) {
+          fiveyText.textContent = 'type some text';
+        } else {
+          fiveyText.textContent = e.target.value;
+        }
+      })
+    }
+
   }
   
   if(document.readyState === 'loading') {
